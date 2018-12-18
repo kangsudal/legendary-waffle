84409c0732f83dfd9a1f1d22148fd30da1b1fe90
kangsudal <kangsudal@gmail.com>
2018-12-18 12:43:07 +0900

    first: store the result in a session(cache the result), ['data']에서 챔피언 이름(key) list로 만들어 출력, second: Making search form. Handing checkbox values when search button is clicked.

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('first', views.first, name='first'),
+    path('second', views.second, name='second'),
 ]
--- core/views.py
+++ core/views.py
@@ -13,12 +13,66 @@ def home(request):
     })
 
 def first(request):
-    response = requests.get('http://ddragon.leagueoflegends.com/cdn/8.13.1/data/en_US/champion.json')
-    geodata = response.json()
-    #print(geodata.keys())
+
+    is_cached = ('champdata' in request.session)
+
+    if not is_cached:
+	    response = requests.get('http://ddragon.leagueoflegends.com/cdn/8.13.1/data/en_US/champion.json')
+	    request.session['champdata'] = response.json()
+
+    champdata = request.session['champdata']
+
+
     return render(request, 'core/first.html', {
-        'type': geodata['type'],
-        'format': geodata['format'],
-        'version': geodata['version'],
-        'data': geodata['data'],
+        'type': champdata['type'],
+        'format': champdata['format'],
+        'version': champdata['version'],
+        'data': champdata['data'],
+
+        'is_cached': is_cached,
+        # add detail data
+        'champion_name': list(champdata['data'].keys()),
+
+    })
+
+def second(request):
+
+    is_cached = ('champdata' in request.session)
+
+    if not is_cached:
+        response = requests.get('http://ddragon.leagueoflegends.com/cdn/8.13.1/data/en_US/champion.json')
+        request.session['champdata'] = response.json()
+
+    champdata = request.session['champdata']
+    print(champdata.keys())
+
+    #search
+    if request.method == 'GET':
+        print('it was get')
+
+        role_query = request.GET.getlist('roles', None)
+        #search_query = request.GET.get('search_box', None)
+
+        if role_query:
+            print(type(role_query))
+        else:
+            print('there was nothing')
+
+
+
+
+
+
+
+
+    return render(request, 'core/second.html', {
+        'type': champdata['type'],
+        'format': champdata['format'],
+        'version': champdata['version'],
+        'data': champdata['data'],
+
+        'is_cached': is_cached,
+        # add detail data
+        'champion_name': list(champdata['data'].keys()),
+
     })
